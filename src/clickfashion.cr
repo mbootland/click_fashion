f8883527da2ca898f29edd00ecfc09148e235847
mbootland <matthew.bootland@gmail.com>
2017-10-12 13:09:05 +0900

    just playing around

--- src/clickfashion.cr
+++ src/clickfashion.cr
@@ -1,5 +1,24 @@
-require "./clickfashion/*"
+require "kemal"
 
-module Clickfashion
-  # TODO Put your code here
+# get "/" do
+#   render "public/index.html"
+# end
+
+# get "/:name" do |env|
+#   name = env.params.url["name"]
+#   render "public/hello.ecr"
+# end
+
+get "/" do |env|
+  render "src/views/subview.ecr", "src/views/layouts/layout.ecr"
 end
+
+Kemal.run
+
+
+
+# require "./clickfashion/*"
+#
+# module Clickfashion
+#   # TODO Put your code here
+# end
